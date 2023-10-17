95db2c75c730d08f99b21bc8e84f195e4d695b77
Rodolfo Torres <rodotc9905@gmail.com>
2023-10-16 21:48:30 -0600

    18.Balanceo de carga del lado del cliente con Ribbon

--- src/main/java/com/formaciondbi/springboot/app/item/clientes/ProductoClienteRest.java
+++ src/main/java/com/formaciondbi/springboot/app/item/clientes/ProductoClienteRest.java
@@ -2,13 +2,12 @@ package com.formaciondbi.springboot.app.item.clientes;
 
 import com.formaciondbi.springboot.app.item.models.Producto;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
-@FeignClient(name = "productos",url="localhost:8001")
+@FeignClient(name = "productos")
 public interface ProductoClienteRest {
 
     @GetMapping("/producto")
